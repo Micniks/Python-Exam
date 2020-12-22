8c6176e7a7286978dc7a51a622768685c2625499
Michael N. Korsgaard <Michael12.korsgaard@gmail.com>
2020-12-22 18:13:50 +0100

    added datasorting and verification

--- MashineLearningTest.py
+++ MashineLearningTest.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import pandas as pd
-import numpy as np
 import argparse
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
